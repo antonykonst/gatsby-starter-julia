e8c5e735508ed494a60f8d92368a87bb4b2e8dbe
Anton <32464271+antonykonst@users.noreply.github.com>
2020-06-24 07:12:17 +0300

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -77,8 +77,8 @@ const Header = ({ siteTitle }) => (
     <Content>
       <p>
         <HomeLink to="/">{siteTitle}</HomeLink>
-        <NavLink to="/blog">Blog</NavLink>
-        <GitHubLink href="https://github.com/niklasmtj/gatsby-starter-julia">
+        <NavLink to="/blog">Site</NavLink>
+        <GitHubLink href="https://github.com/antonykonst">
           GitHub
         </GitHubLink>
       </p>
