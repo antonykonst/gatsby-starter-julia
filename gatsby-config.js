aef8eaae8bde5cc3989b2d2ff1b1daf8dad59a85
Anton <32464271+antonykonst@users.noreply.github.com>
2020-06-24 07:18:40 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Julia Doe`,
-    subtitle: `software developer`,
-    description: `A minimal blog starter built with Gatsbyjs. The needed Gatsby files are included.`,
-    author: `@niklasmtj`,
+    title: `Anton Konstantinov`,
+    subtitle: `Dipl.Ing./M.Eng.`,
+    description: `Hier ist kurze Beschreibung`,
+    author: `@antonykonst`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
